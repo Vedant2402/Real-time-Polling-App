ff14d119278252f267ef0caf2f49374693381119
Vedant Kankate <98166840+Vedant2402@users.noreply.github.com>
2025-04-27 16:32:32 -0700

    Updates

--- polling-app/src/App.jsx
+++ polling-app/src/App.jsx
@@ -2,9 +2,10 @@ import { Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import Create from './pages/Create';
 import { useState } from 'react';
+import { FaSun, FaMoon } from 'react-icons/fa'; // <-- new import
 
 function App() {
-  const [darkMode, setDarkMode] = useState(true);
+  const [darkMode, setDarkMode] = useState(false); // Default Light Mode
 
   const toggleTheme = () => {
     setDarkMode(!darkMode);
@@ -13,13 +14,15 @@ function App() {
   return (
     <div className={darkMode ? 'dark' : 'light'}>
       <nav className="navbar">
-        <div className="nav-links">
+        <div className="nav-left">
           <Link to="/">Home</Link>
           <Link to="/create">Create Poll</Link>
         </div>
-        <button className="toggle-button" onClick={toggleTheme}>
-          {darkMode ? "Light Mode" : "Dark Mode"}
-        </button>
+        <div className="nav-right">
+          <button className="toggle-button" onClick={toggleTheme}>
+            {darkMode ? <FaSun /> : <FaMoon />}
+          </button>
+        </div>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
